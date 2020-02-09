ecec6e769e532fd16364b53f249805c3d315ab96
bernsteining <32954876+bernsteining@users.noreply.github.com>
2020-02-09 19:00:27 +0100

    handle unicode special chars
    unicode special characters are now correctly handled, no more hardcoded characters ...

--- InstaLocTrack.py
+++ InstaLocTrack.py
@@ -48,77 +48,18 @@ parser.add_argument(
 
 args = parser.parse_args()
 
-special_chars = {
-    "u00c0": "À",
-    "u00c1": "Á",
-    "u00c2": "Â",
-    "u00c3": "Ã",
-    "u00c4": "Ä",
-    "u00c5": "Å",
-    "u00c6": "Æ",
-    "u00c7": "Ç",
-    "u00c8": "È",
-    "u00c9": "É",
-    "u00ca": "Ê",
-    "u00cb": "Ë",
-    "u00cc": "Ì",
-    "u00cd": "Í",
-    "u00ce": "Î",
-    "u00cf": "Ï",
-    "u00d1": "Ñ",
-    "u00d2": "Ò",
-    "u00d3": "Ó",
-    "u00d4": "Ô",
-    "u00d5": "Õ",
-    "u00d6": "Ö",
-    "u00d8": "Ø",
-    "u00d9": "Ù",
-    "u00da": "Ú",
-    "u00db": "Û",
-    "u00dc": "Ü",
-    "u00dd": "Ý",
-    "u00df": "ß",
-    "u00e0": "à",
-    "u00e1": "á",
-    "u00e2": "â",
-    "u00e3": "ã",
-    "u00e4": "ä",
-    "u00e5": "å",
-    "u00e6": "æ",
-    "u00e7": "ç",
-    "u00e8": "è",
-    "u00e9": "é",
-    "u00ea": "ê",
-    "u00eb": "ë",
-    "u00ec": "ì",
-    "u00ed": "í",
-    "u00ee": "î",
-    "u00ef": "ï",
-    "u00f0": "ð",
-    "u00f1": "ñ",
-    "u00f2": "ò",
-    "u00f3": "ó",
-    "u00f4": "ô",
-    "u00f5": "õ",
-    "u00f6": "ö",
-    "u00f8": "ø",
-    "u00f9": "ù",
-    "u00fa": "ú",
-    "u00fb": "û",
-    "u00fc": "ü",
-    "u00fd": "ý",
-    "u00ff": "ÿ",
-    "u0153": "œ",
-    "&#x27;": "'",
-}
-
 
 def resolve_special_chars(location):
-    matches = re.findall("(u00[\w+]{2}|&#x27;)", location)
+    matches = re.findall("(u0[\w+]{3}|&#x27;)", location)
     if matches != []:
         for special_char in matches:
-            location = location.replace(special_char,
-                                        special_chars.get(special_char, ""))
+            if special_char != "&#x27;":
+                tmp_char = "\\" + special_char
+                location = location.replace(
+                    special_char,
+                    tmp_char.encode().decode('unicode-escape'))
+            else:
+                location = location.replace(special_char, "'")
     return location
 
 
